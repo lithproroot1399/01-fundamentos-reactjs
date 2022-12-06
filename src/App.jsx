7e2e7b4cd96c004872b76746082db72e9e923ee6
Leandro <prorootinfo@gmail.com>
2022-12-06 16:58:10 -0300

    Adicionado styles no meu projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Post } from "./Post";
 
+import  "./styles.css"
+
 export function App() {
  return (
   <div>
     <Post
       author= "Leandro Savio" 
-      content= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsum labore, 
-      mollitia voluptatibus facere nobis ipsa vel nisi tempora libero molestiae sapiente placeat quidem maxime deleniti esse culpa cupiditate error." 
+      content= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsum labore, mollitia voluptatibus facere nobis ipsa vel nisi tempora libero molestiae sapiente placeat quidem maxime deleniti esse culpa cupiditate error" 
     />
 
     <Post
