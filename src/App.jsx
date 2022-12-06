ecb705056edaab5d4a5a7c8a6e73a0bc475a55fb
Leandro <prorootinfo@gmail.com>
2022-12-06 16:07:36 -0300

    Iniciando meu projeto Reactjs

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Post } from "./Post";
+
+export function App() {
+ return (
+  <div>
+    <Post
+      author= "Leandro Savio" 
+      content= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsum labore, 
+      mollitia voluptatibus facere nobis ipsa vel nisi tempora libero molestiae sapiente placeat quidem maxime deleniti esse culpa cupiditate error." 
+    />
+
+    <Post
+    author= "Caio Marinho"
+    content= "Estou aprendendo SQL"
+  
+    />
+    
+  </div>
+ )
+}
+
+
