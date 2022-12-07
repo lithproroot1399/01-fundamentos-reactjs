d317002cf3e3f5645d1d641fc79b503f0ef6c685
Leandro <prorootinfo@gmail.com>
2022-12-07 15:34:05 -0300

    Concluindo Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,29 @@
+import { PencilLine } from 'phosphor-react' 
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
-            
+            <img 
+            className={styles.cover} 
+            src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
+            />
+
+        <div className={styles.profile}>
+            <img className={styles.avatar} src="https://github.com/lithproroot1399.png" />
+            <strong>Leandro Savio</strong>
+            <span>Web Developer</span>
+        </div>
+
+        <footer>
+            <a href='#'>
+                <PencilLine />
+                Editar sua profile
+            </a>
+
+        </footer>
+
         </aside>
     );
 }
