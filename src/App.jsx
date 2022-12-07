2a9fc242c3f7e05d14dcf7a82b40ad90e5cf44d2
Leandro <prorootinfo@gmail.com>
2022-12-07 08:09:55 -0300

    Adicionando component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 import  "./styles.css"
@@ -5,6 +6,9 @@ import  "./styles.css"
 export function App() {
  return (
   <div>
+
+    <Header />
+
     <Post
       author= "Leandro Savio" 
       content= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsum labore, mollitia voluptatibus facere nobis ipsa vel nisi tempora libero molestiae sapiente placeat quidem maxime deleniti esse culpa cupiditate error" 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
+  import styles from'./Header.module.css'
+
 export function Header() {
     return (
-        <strong>Ignite Fid</strong>
+        <header className={styles.header}>
+            <strong>Ignite Fid</strong>
+        </header>
     )
 }
