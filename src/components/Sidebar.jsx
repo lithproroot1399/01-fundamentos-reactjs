2526ac731fea9ca71419786b2f3e0e77e9903c27
Leandro <prorootinfo@gmail.com>
2022-12-08 12:12:00 -0300

    Alterar imagem do Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img 
             className={styles.cover} 
-            src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" 
+            src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
 
         <div className={styles.profile}>
