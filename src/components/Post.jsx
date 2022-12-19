034c94cf81992a2f9c26e7ad885bf25dee0cbd35
Leandro <prorootinfo@gmail.com>
2022-12-19 09:07:49 -0300

    Adicionando novas funcionalidades

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,33 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post({author, publishedAt}) {
+export function Post({ author, publishedAt, content }) {
+    const [comments] = useState ([
+        1,
+        2,
+    ])
+    
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR, 
+        addSuffix: true,
+    }) 
+
+    function handleCreateNewComment() {
+        event.preventDefault()
+
+        setComments();
+
+        console.log(comments)
+    }
 
     return (
         <article className={styles.post}>
@@ -15,13 +40,20 @@ export function Post({author, publishedAt}) {
                     </div>
                 </div>
 
-            <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:00">
-                {publishedAt.toString()}
+            <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                {publishedDateRelativeNow}
             </time>
         </header>
 
             <div className={styles.content}>
-
+            {content.map(line => {
+                if (line.type === 'paragraph') {
+                    return <p>{line.content}</p>;
+                } else if (line.type === 'link') {
+                    return <p><a href=''>{line.content}</a></p>;
+                }
+                
+            })}
             
             <p>Fala galeraa 👋</p>
             <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
@@ -33,7 +65,7 @@ export function Post({author, publishedAt}) {
                 </p>     
           </div>
 
-        <form className={styles.commentForm}>
+        <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         
             <textarea
@@ -45,9 +77,9 @@ export function Post({author, publishedAt}) {
         </form>
 
         <div className={styles.commentList}>
-          <Comment />
-          <Comment />
-          <Comment />
+          {comments.map(comment => {
+            return <Comment />
+          })}
         </div>
       </article>
     )
